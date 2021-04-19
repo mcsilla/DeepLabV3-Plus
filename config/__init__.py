85aed992492acaae40e3514b0286e6eaaa5c8f74
Majoros <majoroscsilla@arcanumkft.com>
2021-04-19 08:43:20 +0200

    Add test configs

--- config/__init__.py
+++ config/__init__.py
@@ -8,8 +8,12 @@ entry in the CONFIG_MAP as shown.
 import config.camvid_resnet50
 import config.human_parsing_resnet50
 import config.articles_tpu
+import config.articles_tpu_test
+import config.articles_gpu_test
 CONFIG_MAP = {
     'camvid_resnet50': config.camvid_resnet50.CONFIG,
     'human_parsing_resnet50': config.human_parsing_resnet50.CONFIG,
-    'articles': config.articles_tpu.CONFIG
+    'articles_tpu': config.articles_tpu.CONFIG,
+    'articles_tpu_test': config.articles_tpu_test.CONFIG,
+    'articles_gpu_test': config.articles_gpu_test.CONFIG
 }
---
+++ config/articles_gpu_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""Module for training deeplabv3plus on articles."""
+
+from glob import glob
+from pathlib import Path
+import tensorflow as tf
+
+tfrec_train_pattern = 'gs://arcanum-ml/cv/articles/deeplab/test_records/*train*'
+tfrec_val_pattern = 'gs://arcanum-ml/cv/articles/deeplab/test_records/*val*'
+model_dir = 'gs://arcanum-ml/cv/articles/deeplab/model'
+log_dir = 'gs://arcanum-ml/cv/articles/deeplab/model/logs'
+
+CONFIG = {
+    # We mandate specifying project_name and experiment_name in every config
+    # file. They are used for wandb runs if wandb api key is specified.
+    'project_name': 'deeplabv3-plus',
+    'experiment_name': 'articles-segmentation-resnet-50-backbone',
+    'train_dataset_config': {
+        'tf_records': tf.io.gfile.glob(tfrec_train_pattern),
+        'height': 768, 'width': 768, 'batch_size': 1
+    },
+    'val_dataset_config': {
+        'tf_records': tf.io.gfile.glob(tfrec_val_pattern),
+        'height': 768, 'width': 768, 'batch_size': 1
+    },
+    'strategy': 'onedevice',
+    'tpu_name': 'deeplab-articles',
+    'num_classes': 18,
+    'backbone': 'resnet50',
+    'learning_rate': 1e-4,
+    'checkpoint_dir': model_dir,
+    'checkpoint_file_prefix': "ckpt_",
+    'log_dir': log_dir,
+    'epochs': 1,
+    'steps_per_epoch': 40,
+    'validation_steps': 5,
+}
