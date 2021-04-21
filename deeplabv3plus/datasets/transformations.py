11c9f9dbfa55e705b33a3a7395aad5930d1eb079
Majoros <majoroscsilla@arcanumkft.com>
2021-04-21 14:41:49 +0200

    Try fix random

--- deeplabv3plus/datasets/transformations.py
+++ deeplabv3plus/datasets/transformations.py
@@ -1,36 +1,33 @@
 import tensorflow as tf
 import numpy as np
 
-@tf.function
+
 def random_brightness(image, label):
     seed = tf.random.uniform(
         [2], minval=0, maxval=10, dtype=tf.dtypes.int32, seed=None, name=None
     )
     return tf.image.stateless_random_brightness(image, max_delta=0.1, seed=seed), label
 
-@tf.function
 def random_contrast(image, label):
     seed = tf.random.uniform(
         [2], minval=0, maxval=10, dtype=tf.dtypes.int32, seed=None, name=None
     )
     return tf.image.stateless_random_contrast(image, lower=0.9, upper=1.5, seed=seed), label
 
-@tf.function
 def random_crop(image, label):
-    fraction = tf.random.uniform(
-        [], minval=0.95, maxval=1, dtype=tf.dtypes.float32, seed=None, name=None
-    )
+    fraction = np.random.uniform(0.95, 1)
+    # fraction = tf.random.uniform(
+    #     [], minval=0.95, maxval=1, dtype=tf.dtypes.float32, seed=None, name=None
+    # )
     return (tf.image.central_crop(image, central_fraction=fraction), 
             tf.image.central_crop(label, central_fraction=fraction))
 
-@tf.function
 def random_hue(image, label):
     seed = tf.random.uniform(
         [2], minval=0, maxval=10, dtype=tf.dtypes.int32, seed=None, name=None
     )
     return tf.image.stateless_random_hue(image, max_delta=0.08, seed=seed), label
 
-@tf.function
 def random_saturation(image, label):
     seed = tf.random.uniform(
         [2], minval=0, maxval=10, dtype=tf.dtypes.int32, seed=None, name=None
@@ -41,17 +38,10 @@ def make_gray(image, label):
     gray_image = tf.image.rgb_to_grayscale(image)
     return tf.image.grayscale_to_rgb(gray_image), label
 
-@tf.function
 def make_random_transformation(image, label):
-    rand = tf.random.uniform(
-        [], minval=0, maxval=1, dtype=tf.dtypes.float32, seed=None, name=None
-    )
-    rand_crop = tf.random.uniform(
-        [], minval=0, maxval=1, dtype=tf.dtypes.float32, seed=None, name=None
-    )
-    rand_grey = tf.random.uniform(
-        [], minval=0, maxval=1, dtype=tf.dtypes.float32, seed=None, name=None
-    )
+    rand = np.random.uniform(0, 1)
+    rand_crop = np.random.uniform(0, 1)
+    rand_grey = np.random.uniform(0, 1)
     if rand_crop < 0.05:
         image, label = random_crop(image, label)
     if rand_grey < 0.02:
