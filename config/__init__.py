830e2508829133c038742ad7deef7176e8f547ae
Majoros <majoroscsilla@arcanumkft.com>
2021-08-07 18:34:39 +0200

    Fix typo

--- config/__init__.py
+++ config/__init__.py
@@ -10,13 +10,13 @@ import config.human_parsing_resnet50
 import config.articles_tpu
 import config.articles_tpu_test
 import config.articles_gpu_test
-import config.articles_segmentation
+import config.article_segmentation
 
 CONFIG_MAP = {
     'camvid_resnet50': config.camvid_resnet50.CONFIG,
     'human_parsing_resnet50': config.human_parsing_resnet50.CONFIG,
     'articles_tpu': config.articles_tpu.CONFIG,
-    'articles_segmentation': config.articles_segmentation.CONFIG,
+    'articles_segmentation': config.article_segmentation.CONFIG,
     'articles_tpu_test': config.articles_tpu_test.CONFIG,
     'articles_gpu_test': config.articles_gpu_test.CONFIG
 }
